76021d53d663b4a262c15fd00c55b059feaa4954
Felix <gomezrodriguezjosefelix@gmail.com>
2020-09-02 18:52:08 +0200

    Nuevo proyecto spring boot

---
+++ ejemplo/src/test/java/fel/dev/spring/react/ejemplo/EjemploApplicationTests.java
@@ -0,0 +1,13 @@
+package fel.dev.spring.react.ejemplo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjemploApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
